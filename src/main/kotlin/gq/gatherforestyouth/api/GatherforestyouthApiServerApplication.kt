40e50f7067d9293d8357c716a454e1b540fa630c
sprtms16 <sprtms16@jeit.co.kr>
2021-11-13 01:13:31 +0900

    init project

---
+++ src/main/kotlin/gq/gatherforestyouth/api/GatherforestyouthApiServerApplication.kt
@@ -0,0 +1,11 @@
+package gq.gatherforestyouth.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatherforestyouthApiServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatherforestyouthApiServerApplication>(*args)
+}
---
+++ src/test/kotlin/gq/gatherforestyouth/api/GatherforestyouthApiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package gq.gatherforestyouth.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatherforestyouthApiServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
